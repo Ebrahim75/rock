1276d2ed3acd920939c4bdeb78bb31b53e29e8bf
ebrahim <45916275+Ebrahim75@users.noreply.github.com>
2018-12-21 01:52:30 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,37 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+client.on('ready', async() => {
+var server = "514245515836260372"; 
+var channel = "514245515836260374";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , سبحان الله . الله اكبر . اشهد ان لا اله الا الله , **')
+    },305);
+})
+
+client.on('message', message => {
+        var prefix = "-";
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+
+
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + '1s')) {
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+
+
+      });
+
+ 
+client.login(process.env.BOT_TOKEN);
